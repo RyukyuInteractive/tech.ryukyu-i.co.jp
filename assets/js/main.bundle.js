d9b38c0bdb3fcdf71eece2eb653a1bc6c3a800f0
jhonyspicy <jhonyspicy@gmail.com>
2017-08-23 16:26:51 +0900

    update and fix webpack error

--- assets/js/main.bundle.js
+++ assets/js/main.bundle.js
@@ -1,53 +1,56 @@
-webpackJsonp([0],[
-/* 0 */
+webpackJsonp([0],{
+
+/***/ 89:
 /***/ (function(module, exports, __webpack_require__) {
 
-	/* WEBPACK VAR INJECTION */(function($, Velocity) {'use strict';
+"use strict";
+/* WEBPACK VAR INJECTION */(function($, Velocity) {
 
-	$(function () {
-	  // Logo
-	  Velocity({
-	    e: $('.logo'),
-	    p: {
-	      opacity: [1, 0],
-	      translateX: ['0px', '20px']
-	    },
-	    o: {
-	      delay: 100,
-	      duration: 2000,
-	      easing: 'easeOutExpo'
-	    }
-	  });
+$(function () {
+    // Logo
+    Velocity({
+        e: $('.logo'),
+        p: {
+            opacity: [1, 0],
+            translateX: ['0px', '20px']
+        },
+        o: {
+            delay: 100,
+            duration: 2000,
+            easing: 'easeOutExpo'
+        }
+    });
 
-	  // Intro
-	  Velocity({
-	    e: $('.intro .lead'),
-	    p: {
-	      opacity: [1, 0],
-	      translateY: ['0px', '-20px']
-	    },
-	    o: {
-	      easing: 'easeOutQuint',
-	      stagger: 500,
-	      duration: 1500,
-	      delay: 300,
-	      complete: function complete() {
-	        Velocity({
-	          e: document.querySelector('.intro'),
-	          p: {
-	            backgroundColor: ['#000', '#fff'],
-	            color: ['#ffffff', '#464646']
-	          },
-	          o: {
-	            duration: 1500,
-	            easing: 'easeInOutQuint'
-	          }
-	        });
-	      }
-	    }
-	  });
-	});
-	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1), __webpack_require__(2)))
+    // Intro
+    Velocity({
+        e: $('.intro .lead'),
+        p: {
+            opacity: [1, 0],
+            translateY: ['0px', '-20px']
+        },
+        o: {
+            easing: 'easeOutQuint',
+            stagger: 500,
+            duration: 1500,
+            delay: 300,
+            complete: function complete() {
+                Velocity({
+                    e: document.querySelector('.intro'),
+                    p: {
+                        backgroundColor: ['#000', '#fff'],
+                        color: ['#ffffff', '#464646']
+                    },
+                    o: {
+                        duration: 1500,
+                        easing: 'easeInOutQuint'
+                    }
+                });
+            }
+        }
+    });
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(21), __webpack_require__(38)))
 
 /***/ })
-]);
+
+},[89]);
--- webpack.config.js
+++ webpack.config.js
@@ -2,71 +2,51 @@ import webpack from 'webpack';
 import path from 'path'
 
 module.exports = {
-  entry: {
-    'main': `${__dirname}/_dev/js/main`,
-    'vendor': [
-      'three',
-      'gsap',
-      'pixi.js',
-      'velocity-animate',
-    ]
-  },
-  output: {
-    path: `${__dirname}/assets/js/`,
-    filename: '[name].bundle.js'
-  },
-  node: {
-    fs: 'empty'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        include: path.join(__dirname, 'node_modules', 'pixi.js'),
-        loader: 'json',
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      }
+    entry: {
+        'main': `${__dirname}/_dev/js/main`,
+        'vendor': [
+            'three',
+            'gsap',
+            'pixi.js',
+            'velocity-animate',
+        ]
+    },
+    output: {
+        path: `${__dirname}/assets/js/`,
+        filename: '[name].bundle.js'
+    },
+    node: {
+        fs: 'empty'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.json$/,
+                include: path.join(__dirname, 'node_modules', 'pixi.js'),
+                loader: 'json',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                include: path.resolve(__dirname, 'node_modules/pixi.js'),
+                loader: 'ify-loader'
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            jQuery: 'jquery',
+            $: 'jquery',
+            "window.jQuery": "jquery",
+            THREE: 'three',
+            PIXI: 'pixi.js',
+            TweenMax: 'gsap',
+            Velocity: 'velocity-animate',
+        }),
+        new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js'}),
     ],
-    postLoaders: [
-      {
-        include: path.resolve(__dirname, 'node_modules/pixi.js'),
-        loader: 'ify'
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      jQuery: 'jquery',
-      $: 'jquery',
-      "window.jQuery": "jquery",
-      THREE: 'three',
-      PIXI: 'pixi.js',
-      TweenMax: 'gsap',
-      Velocity: 'velocity-animate',
-    }),
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    /*
-     *new webpack.optimize.OccurenceOrderPlugin(),
-     *new webpack.DefinePlugin({
-     *  'process.env': {
-     *    'NODE_ENV': JSON.stringify('production')
-     *  }
-     *}),
-     */
-    /*
-     *new webpack.optimize.UglifyJsPlugin({
-     *  output: {
-     *    comments: '@license',
-     *  },
-     *  compressor: {
-     *    warnings: false
-     *  }
-     *})
-     */
-  ],
 };
 
