a54df215ee2d73c667649ba7aa0438fbd02f0e44
ameyamashiro <yamashiro@ryukyu-i.co.jp>
2018-10-23 11:19:50 +0900

    fix some vulnerabilities and update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,87 +1,89 @@
-import path from 'path';
-import gulp from 'gulp';
-import sass from 'gulp-sass';
-import browserSync from 'browser-sync';
-import webpack from 'webpack-stream';
-import plumber from 'gulp-plumber';
-import bourbon from 'node-bourbon';
-import { spawn } from 'child_process';
-import webpackConfig from './webpack.config.js';
+import path from "path";
+import gulp from "gulp";
+import sass from "gulp-sass";
+import browserSync from "browser-sync";
+import webpack from "webpack-stream";
+import plumber from "gulp-plumber";
+import bourbon from "node-bourbon";
+import { spawn } from "child_process";
+import webpackConfig from "./webpack.config.js";
 
 let messages = {
-  jekyllBuild: '<span style="color: grey">Running:</span> $ bundle exec jekyll build'
+  jekyllBuild:
+    '<span style="color: grey">Running:</span> $ bundle exec jekyll build'
 };
 
 const paths = {
-  baseDir: '_site',
-  srcDir: '_dev',
-  destDir: 'assets',
-  js: 'js/**/*.js',
-  sass: 'css/**/*.scss',
+  baseDir: "_site",
+  srcDir: "_dev",
+  destDir: "assets",
+  js: "js/**/*.js",
+  sass: "css/**/*.scss",
   html: [
-    '*.html',
-    '_includes/*.html',
-    '_layouts/*.html',
-    '_posts/**/*',
-    '_npm/**/*'
+    "*.html",
+    "_includes/*.html",
+    "_layouts/*.html",
+    "_posts/**/*",
+    "_npm/**/*"
   ]
 };
 
 const opts = {
   sass: {
-    outputStyle: 'expanded',
+    outputStyle: "expanded",
     includePaths: bourbon.includePaths
   },
   browserSync: {
     server: {
       baseDir: paths.baseDir
     },
-    browser: 'default',
-    open: false,
+    browser: "default",
+    open: false
   },
   webpack: webpackConfig
 };
 
-gulp.task('jekyll-build', done => {
+export const jekyllBuildTask = done => {
   browserSync.notify(messages.jekyllBuild);
-  return spawn('bundle' , ['exec', 'jekyll', 'build'], { stdio: 'inherit' })
-    .on('close', done);
-});
+  return spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" }).on(
+    "close",
+    done
+  );
+};
 
-gulp.task('jekyll-rebuild', ['jekyll-build'], () => {
-  browserSync.reload();
-});
-
-gulp.task('browser-sync', ['sass', 'webpack', 'jekyll-build'], () => {
-  browserSync.init(null, opts.browserSync);
-});
-
-gulp.task('sass', () => {
-  return gulp.src(path.join(paths.srcDir, paths.sass), { base: paths.srcDir })
+export const sassTask = () => {
+  return gulp
+    .src(path.join(paths.srcDir, paths.sass), { base: paths.srcDir })
     .pipe(plumber())
-    .pipe(sass(opts.sass).on('error', sass.logError))
+    .pipe(sass(opts.sass).on("error", sass.logError))
     .pipe(gulp.dest(path.join(paths.baseDir, paths.destDir)))
     .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest(paths.destDir))
-});
+    .pipe(gulp.dest(paths.destDir));
+};
 
-gulp.task('webpack', () => {
-  return gulp.src(path.join(paths.srcDir, paths.js))
+export const webpackTask = () => {
+  return gulp
+    .src(path.join(paths.srcDir, paths.js))
     .pipe(plumber())
     .pipe(webpack(opts.webpack))
-    .pipe(gulp.dest(path.join(paths.baseDir, paths.destDir, '/js')))
+    .pipe(gulp.dest(path.join(paths.baseDir, paths.destDir, "/js")))
     .pipe(browserSync.reload({ stream: true }))
-    .pipe(gulp.dest(path.join(paths.destDir, '/js')));
-});
+    .pipe(gulp.dest(path.join(paths.destDir, "/js")));
+};
 
-gulp.task('watch', () => {
-  gulp.watch(path.join(paths.srcDir, paths.sass), ['sass']);
-  gulp.watch(path.join(paths.srcDir, paths.js), ['webpack']);
-  gulp.watch(paths.html, ['jekyll-rebuild']);
-});
+export const watch = () => {
+  gulp.watch(path.join(paths.srcDir, paths.sass), gulp.series(sassTask));
+  gulp.watch(path.join(paths.srcDir, paths.js), gulp.series(webpackTask));
+  gulp.watch(paths.html, gulp.series(jekyllBuildTask));
+};
 
-gulp.task('default', [
-  'browser-sync',
-  'watch',
-]);
+export const browserSyncTask = gulp.series(
+  sassTask,
+  webpackTask,
+  jekyllBuildTask,
+  () => {
+    browserSync.init(null, opts.browserSync);
+  }
+);
 
+export default gulp.series(browserSyncTask, watch);
