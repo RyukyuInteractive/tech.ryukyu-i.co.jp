5d85e2afc8a1e9b38a5d5e524fa9fa45b49eb115
reiwa <reiwa@outlook.com.vn>
2023-12-05 15:51:00 +0900

    update: レイアウトを変更する

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,6 +1,5 @@
 import { PostCard } from "@/app/_components/post-card"
 import { Badge } from "@/components/ui/badge"
-import { Separator } from "@/components/ui/separator"
 import { config } from "@/config"
 import { getPostTags } from "@/lib/markdown/get-post-tags"
 import { getTagPosts } from "@/lib/markdown/get-tag-posts"
@@ -27,8 +26,7 @@ const TagPage = async (props: Props) => {
           </Link>
         ))}
       </nav>
-      <Separator />
-      <h2 className="text-2xl">{`タグ「${props.params.tag}」に関連する記事`}</h2>
+      <h2 className="text-2xl font-bold">{`タグ「${props.params.tag}」に関連する記事`}</h2>
       <section className="space-y-4">
         {posts.map((post) => (
           <article key={post.slug}>
